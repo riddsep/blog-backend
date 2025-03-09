013ffe992fd368a74b52ff48747b58844c26b4e0
riddsep <ridoseptiawan00@gmail.com>
2025-03-09 17:59:19 +0700

    minor changes

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,24 @@ const totalLikes = (blogList) => {
   return blogList.reduce((acc, item) => acc + item.likes, 0);
 };
 
+const favoritBlog = (blogList) => {
+  const favorite = blogList.reduce((fav, blog) => {
+    return blog.likes > fav.likes ? blog : fav;
+  }, blogList[0]);
+  return favorite;
+};
+
+const mostLikes = (blogList) => {
+  const { author, likes } = blogList.reduce(
+    (fav, blog) => (blog.likes > fav.likes ? blog : fav),
+    blogList[0]
+  );
+  return { author, likes };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoritBlog,
+  mostLikes,
 };
